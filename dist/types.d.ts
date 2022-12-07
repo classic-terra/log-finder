9a7baf9a3f8ab97c89ac6833914607b160845b8a
echel0n <echel0n@sickrage.ca>
2022-12-07 13:21:28 -0800

    bumped version

--- dist/types.d.ts
+++ dist/types.d.ts
@@ -6,10 +6,10 @@ export interface Attribute {
     key: string;
     value: string;
 }
-export declare type Attributes = Attribute[];
-export declare type LogFinderFn = (log: LogFragment, match: LogFragment['attributes']) => void;
-export declare type LogFinderMapper = (log: LogFragment) => void;
-export declare type LogFinderRuleItem = string | ((value: string) => boolean);
+export type Attributes = Attribute[];
+export type LogFinderFn = (log: LogFragment, match: LogFragment['attributes']) => void;
+export type LogFinderMapper = (log: LogFragment) => void;
+export type LogFinderRuleItem = string | ((value: string) => boolean);
 export interface LogFinderRule {
     type: string;
     attributes: LogFinderRuleItem[][];
@@ -17,12 +17,12 @@ export interface LogFinderRule {
     matchUntil?: string;
     chunkBy?: true;
 }
-export declare type ReturningLogFinderResult<T> = {
+export type ReturningLogFinderResult<T> = {
     fragment: LogFragment;
     match: LogFragment['attributes'];
     height?: number;
     transformed?: T;
     chunk?: Record<string, LogFragment['attributes'][]>;
 };
-export declare type ReturningLogFinderTransformer<T> = (fragment: LogFragment, match: LogFragment['attributes']) => T;
-export declare type ReturningLogFinderMapper<T> = (event: LogFragment) => ReturningLogFinderResult<T>[];
+export type ReturningLogFinderTransformer<T> = (fragment: LogFragment, match: LogFragment['attributes']) => T;
+export type ReturningLogFinderMapper<T> = (event: LogFragment) => ReturningLogFinderResult<T>[];
